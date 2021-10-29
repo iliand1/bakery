86938f71810ff0529c57ce1bd41f5b42b9730875
iliand <iliandaviaud1@gmail.com>
2021-10-29 21:46:25 +0300

    mobile finish

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 let width = window.screen.width;
 console.log(width);
 if (width < 600) {
-  document.querySelector(".menu").remove();
-  document.querySelector(".fas").addEventListener("click", () => {});
-}
 
-new Hiraku(".offcanvas-right", {
-	btn: "#offcanvas-btn-right",
-	fixedHeader: "#header",
-	direction: "right"
-  });
+  document.querySelector(".menu").remove();
+  new Hiraku(".offcanvas-right", {
+  btn: "#offcanvas-btn-right",
+	  fixedHeader: "#header",
+	  direction: "right",
+	});
+}
+function openNav() {
+	document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+	document.getElementById("mySidenav").style.width = "0px";
+  } 
