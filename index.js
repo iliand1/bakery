a1fbcba66080c89addc09d20ed72fb3eda2e6897
iliand <iliandaviaud1@gmail.com>
2021-11-04 22:31:21 +0300

    webstorm

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let width = window.screen.width;
 console.log(width);
 if (width < 600) {
-  document.querySelector(".menu").remove();
+    document.querySelector(".menu").remove();
 }
+
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("mySidenav").style.width = "0px";
 }
