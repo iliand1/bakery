503972a40e34e84ec4c2ef958cd8d895229bbe06
iliand <iliandaviaud1@gmail.com>
2021-11-15 19:44:40 +0300

    about section

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let width = window.screen.width;
 console.log(width);
 if (width < 600) {
-    document.querySelector(".menu").remove();
+  document.querySelector(".menu").remove();
+  document.querySelector(".social-media-img").remove();
 }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
+  document.getElementById("mySidenav").style.width = "0px";
 }
 /*new fullpage('#fullpage', {
     autoScrolling:true,
