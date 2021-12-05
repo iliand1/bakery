2d5edd487ffa4ff0f901ef146f94f7fb8058e282
iliand <iliandaviaud1@gmail.com>
2021-12-05 22:10:40 +0300

    menu

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 let width = window.screen.width;
-console.log(width);
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0px";
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
 if (width < 600) {
   document.querySelector(".menu").remove();
   document.querySelector(".social-media-img").remove();
 }
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0px";
-}
 /*new fullpage('#fullpage', {
     autoScrolling:true,
     scrollHorizontally: true
