0c1c7cd817c128fc6ad09ead052e01a111d9f281
iliand <iliandaviaud1@gmail.com>
2021-12-07 22:35:00 +0300

    finished desktop!

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 let width = window.screen.width;
+
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0px";
 }
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
 }
+
 if (width < 600) {
   document.querySelector(".menu").remove();
   document.querySelector(".social-media-img").remove();
 }
-
-/*new fullpage('#fullpage', {
-    autoScrolling:true,
-    scrollHorizontally: true
-});*/
